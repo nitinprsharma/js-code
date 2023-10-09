7595f375b3021b5df4d45eda0a8f132c9c0686ef
Nitin prakash sharma <42340064+nitinprsharma@users.noreply.github.com>
2023-10-09 04:59:43 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)   //we are using nodeJS, note browser
+
+console.log("nitin")
+
+console.log(3+3)
+
+//Data types
+
+let name = "Nitin"
+let age = 21
+let isLoggedIn = false
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalon value
+// undefined = 
+// symbol => unique
+
+// object
+
+console.log(typeof "Nitin");  //string
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
