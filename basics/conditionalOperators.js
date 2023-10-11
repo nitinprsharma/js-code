157fb94bb2f1da770a64b3315213cbbdd623c5a3
Nitin prakash sharma <42340064+nitinprsharma@users.noreply.github.com>
2023-10-11 13:18:28 +0000

    conditional

---
+++ basics/conditionalOperators.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);  //true
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(undefined == 2);
+// console.log(undefined > 2);
+// console.log(undefined < 2);
+
+//typescript dont allow to compare two different type data types
+
+//  ===
+
+
+console.log("2" ===2);
