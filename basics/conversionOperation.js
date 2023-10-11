5ffe3837c1af35c4b5d1faa21432461215b8ad60
Nitin prakash sharma <42340064+nitinprsharma@users.noreply.github.com>
2023-10-11 13:01:10 +0000

    conversionOperation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,70 @@
+// let score=33
+
+// console.log(score)
+// console.log(typeof score)
+
+// let value = String(score)
+// console.log(value)
+// console.log(typeof value)
+
+// let score = null
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let score = "nitin"
+// console.log(score)
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//Operation
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Nitin"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2)  //It will wor as string
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //It will wor as string
+// console.log(1 +2 + "2");
+
+// If we define string first then it take all as string OR if we take number first then it will treate number and string both as define.
+
+// console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+// console.log(num1,num2, num3);
+let gameCounter = 100
+gameCounter++
+
+console.log(gameCounter)
+
+
+
+
+
+
+
